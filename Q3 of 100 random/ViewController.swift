6cb27c0a5ee3697efbd12f378135ebaeb9034b84
ryan_chang <k876541@hotmail.com>
2021-02-20 11:49:42 +0800

    Initial Commit

---
+++ Q3 of 100 random/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Q3 of 100 random
+//
+//  Created by Ryan Chang on 2021/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
